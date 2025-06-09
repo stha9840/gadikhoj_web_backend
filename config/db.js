f2a1fae199780a1184dac16e8c535c4684f7e52b
stha9840 <aayushstha610@gmail.com>
2025-06-09 19:51:45 +0545

    user model added

--- config/db.js
+++ config/db.js
@@ -1,19 +1,15 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const CONNECTION_STRING = process.env.MONGODB_URL
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log(" MongoDB connected successfully");
+  } catch (error) {
+    console.error("DB error", error);
+  }
+};
 
-const connectDB = async()=>{
-    try{
-        await mongoose.connect(
-            CONNECTION_STRING,
-            {
-                useNewUrlParser: true,
-                useUnifiedTopology: true
-            }
-        )
-        console.log("Mongodb Connected");
-    }catch(err){
-        console.log("DB error", err);
-    }
-}
-module.exports = connectDB
+module.exports = connectDB;
--- index.js
+++ index.js
@@ -1,14 +1,29 @@
+// Import core dependencies
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 
-dotenv.config();  
+// Load environment variables from .env file
+require('dotenv').config();
 
+
+// Connect to MongoDB
 connectDB();
 
+// Initialize Express app
 const app = express();
+
+// Middleware to parse incoming JSON requests
+app.use(express.json());
+
+// Load Routes
+const userRoute = require("./routes/userRoute"); 
+// Mount routes with prefix
+app.use("/api/auth", userRoute) 
+// Define server port
 const PORT = process.env.PORT || 5000;
 
+// Start server
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
