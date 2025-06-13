19b149629414637feb18d60e5623cbfdfd64e03b
stha9840 <aayushstha610@gmail.com>
2025-06-13 19:53:00 +0545

    register route setup

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,6 +6,6 @@ const {registerUser, loginUser} = require("../controllers/userController");
 
 // Define routes
 router.post('/register', registerUser);
-router.post('/login', loginUser)
+
 
 module.exports = router;
