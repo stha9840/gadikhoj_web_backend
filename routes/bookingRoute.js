3dc4b0bccdc9fb27ac900457348300a124bb4cad
stha9840 <aayushstha610@gmail.com>
2025-06-29 11:32:10 +0545

    update and delete route added

--- routes/bookingRoute.js
+++ routes/bookingRoute.js
@@ -3,12 +3,22 @@ const router = express.Router();
 const bookingController = require("../controllers/bookingController");
 const { authenticateUser } = require("../middlewares/authenticateUser");
 
-// If you don't have auth middleware yet, you can omit verifyToken for now.
-// Later, add it to protect these routes.
+// Create a booking
+router.post("/", authenticateUser, bookingController.createBooking);
 
-router.post("/", authenticateUser,  bookingController.createBooking);        // Create booking
-router.get("/my",authenticateUser, bookingController.getUserBookings);     // Get bookings of logged-in user
-router.get("/", bookingController.getAllBookings);        // Admin: get all bookings
-router.patch("/:bookingId/cancel", bookingController.cancelBooking);  // Cancel booking
+// Get bookings of logged-in user
+router.get("/my", authenticateUser, bookingController.getUserBookings);
+
+// Get all bookings (Admin)
+router.get("/", bookingController.getAllBookings);
+
+// Cancel a booking
+router.patch("/:bookingId/cancel", authenticateUser, bookingController.cancelBooking);
+
+//  Update a booking
+router.patch("/:bookingId", authenticateUser, bookingController.updateBooking);
+
+//  Delete a booking
+router.delete("/:bookingId", authenticateUser, bookingController.deleteBooking);
 
 module.exports = router;
